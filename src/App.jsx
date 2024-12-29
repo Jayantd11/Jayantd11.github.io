3d864f62aeb4461daee648ad84e7e3dede30adc7
Jayantd11 <130610628+Jayantd11@users.noreply.github.com>
2024-12-29 02:05:26 -0500

    type shit 3d models

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from './sections/navbar.jsx';
+import Hero from './sections/Hero.jsx';
+
 
 const App = () => {
   return(
-    <main className="max-w-7xl max-auto relative">
+    <main className="max-w-10xl max-auto relative">
       <Navbar />
+      <Hero />
       </main>
   );
 };
