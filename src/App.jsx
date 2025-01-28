e3284b8e88f8f07e2fc89b6af55e3c16baea994c
Jayantd11 <130610628+Jayantd11@users.noreply.github.com>
2025-01-27 21:57:50 -0500

    about me

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from './sections/navbar.jsx';
 import Hero from './sections/Hero.jsx';
+import About from './sections/about.jsx';
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <main className="max-w-10xl max-auto relative">
       <Navbar />
       <Hero />
+      <About />
       </main>
   );
 };
